2936d31250543255b9046b6a564460805c3e4b35
Ayush-khelwal2003 <ayushkhelwal2003@gmail.com>
2025-07-15 15:28:39 +0530

    Task 2 done

--- ayush_khelwal/Task-1/index.js
+++ ayush_khelwal/Task-1/index.js
@@ -1,13 +1,21 @@
 
 const express = require('express');
+const {default:mongoose} = require('mongoose');
 
 const app = express();
-const Port = 3005;
+const Port = 3003;
 const aboutRouter = require('./routes/aboutRoute')
+const mongo_url = "mongodb://localhost/3003"
 
 app.use(aboutRouter);
 
+
+mongoose.connect(mongo_url).then(()=> {
+  console.log("mongodb is connected");
 app.listen(Port,()=>{
   console.log(`Server is running on port ${Port}`);
 }
 )
+}).catch(error => {
+  console.log("mongodb is not connected",error);
+})
---
+++ ayush_khelwal/Task-2/index.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const {default:mongoose} = require('mongoose');
+
+const app = express();
+const Port = 3003;
+const aboutRouter = require('./routes/aboutRoute')
+const mongo_url ="mongodb://localhost:27017/Taskify"
+
+app.use(aboutRouter);
+
+
+mongoose.connect(mongo_url).then(()=> {
+  console.log("mongodb is connected");
+app.listen(Port,()=>{
+  console.log(`Server is running on port ${Port}`);
+}
+)
+}).catch(error => {
+  console.log("mongodb is not connected",error);
+})
